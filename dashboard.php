3227afaa7ff7ff95d06019ec5752cb3856931065
okki23 <okkisetyawan@gmail.com>
2016-03-27 16:00:48 +0700

    template crud

--- dashboard.php
+++ dashboard.php
@@ -3,5 +3,5 @@ echo "anda berhasil login";
 echo "<br>";
  
 ?>
-
+<a href="master_siswa.php"> Master Siswa</a>
 <a href="index.php"> Logout </a>
---
+++ master_siswa.php
@@ -0,0 +1,341 @@
+<!DOCTYPE html>
+<html class="no-js"> 
+    <head>
+        <meta charset="utf-8">
+
+        <title>Master Siswa</title>
+
+      
+        <link rel="shortcut icon" href="asset/img/favicon.ico">
+     
+        
+        <link rel="stylesheet" href="asset/css/bootstrap.min.css">
+
+     
+        <link rel="stylesheet" href="asset/css/plugins.css">
+
+        
+        <link rel="stylesheet" href="asset/css/main.css">
+
+        
+        <link rel="stylesheet" href="asset/css/themes.css">
+        
+        <script src="asset/js/vendor/modernizr-2.7.1-respond-1.4.2.min.js"></script>
+    </head>
+    
+    <body>
+       
+        <div class="preloader themed-background">
+            <h1 class="push-top-bottom text-light text-center"><strong>Pro</strong>UI</h1>
+            <div class="inner">
+                <h3 class="text-light visible-lt-ie9 visible-lt-ie10"><strong>Loading..</strong></h3>
+                <div class="preloader-spinner hidden-lt-ie9 hidden-lt-ie10"></div>
+            </div>
+        </div>
+        
+        <div id="page-container" class="sidebar-partial sidebar-visible-lg sidebar-no-animations">
+            <!-- Alternative Sidebar -->
+            <div id="sidebar-alt">
+                <!-- Wrapper for scrolling functionality -->
+              
+                <!-- END Wrapper for scrolling functionality -->
+            </div>
+            <!-- END Alternative Sidebar -->
+
+            <!-- Main Sidebar -->
+            <div id="sidebar">
+                <!-- Wrapper for scrolling functionality -->
+                <div class="sidebar-scroll">
+                    <!-- Sidebar Content -->
+                    <div class="sidebar-content">
+                        <!-- Brand -->
+                        <a href="index.html" class="sidebar-brand">
+                            <i class="gi gi-flash"></i><strong>Pro</strong>UI
+                        </a>
+                        <!-- END Brand -->
+
+                        
+
+                        <!-- Theme Colors -->
+                        <!-- Change Color Theme functionality can be found in js/app.js - templateOptions() -->
+                        <ul class="sidebar-section sidebar-themes clearfix">
+                            <li class="active">
+                                <a href="javascript:void(0)" class="themed-background-dark-default themed-border-default" data-theme="default" data-toggle="tooltip" title="Default Blue"></a>
+                            </li>
+                            <li>
+                                <a href="javascript:void(0)" class="themed-background-dark-night themed-border-night" data-theme="css/themes/night.css" data-toggle="tooltip" title="Night"></a>
+                            </li>
+                            <li>
+                                <a href="javascript:void(0)" class="themed-background-dark-amethyst themed-border-amethyst" data-theme="css/themes/amethyst.css" data-toggle="tooltip" title="Amethyst"></a>
+                            </li>
+                            <li>
+                                <a href="javascript:void(0)" class="themed-background-dark-modern themed-border-modern" data-theme="css/themes/modern.css" data-toggle="tooltip" title="Modern"></a>
+                            </li>
+                            <li>
+                                <a href="javascript:void(0)" class="themed-background-dark-autumn themed-border-autumn" data-theme="css/themes/autumn.css" data-toggle="tooltip" title="Autumn"></a>
+                            </li>
+                            <li>
+                                <a href="javascript:void(0)" class="themed-background-dark-flatie themed-border-flatie" data-theme="css/themes/flatie.css" data-toggle="tooltip" title="Flatie"></a>
+                            </li>
+                            <li>
+                                <a href="javascript:void(0)" class="themed-background-dark-spring themed-border-spring" data-theme="css/themes/spring.css" data-toggle="tooltip" title="Spring"></a>
+                            </li>
+                            <li>
+                                <a href="javascript:void(0)" class="themed-background-dark-fancy themed-border-fancy" data-theme="css/themes/fancy.css" data-toggle="tooltip" title="Fancy"></a>
+                            </li>
+                            <li>
+                                <a href="javascript:void(0)" class="themed-background-dark-fire themed-border-fire" data-theme="css/themes/fire.css" data-toggle="tooltip" title="Fire"></a>
+                            </li>
+                        </ul>
+                        <!-- END Theme Colors -->
+
+                        <!-- Sidebar Navigation -->
+                        <ul class="sidebar-nav">
+                            
+                            <li>
+                                <a href="#" class="sidebar-nav-menu"><i class="fa fa-angle-left sidebar-nav-indicator"></i><i class="gi gi-certificate sidebar-nav-icon"></i>Menu Master</a>
+                                <ul>
+                                    <li>
+                                        <a href="page_ui_grid_blocks.html">Grid &amp; Blocks</a>
+                                    </li>
+                                    <li>
+                                        <a href="page_ui_draggable_blocks.html">Draggable Blocks</a>
+                                    </li>
+                                    <li>
+                                        <a href="page_ui_typography.html">Typography</a>
+                                    </li>
+                                    <li>
+                                        <a href="page_ui_buttons_dropdowns.html">Buttons &amp; Dropdowns</a>
+                                    </li>
+                                    <li>
+                                        <a href="page_ui_navigation_more.html">Navigation &amp; More</a>
+                                    </li>
+                                    <li>
+                                        <a href="page_ui_horizontal_menu.html">Horizontal Menu</a>
+                                    </li>
+                                    <li>
+                                        <a href="page_ui_progress_loading.html">Progress &amp; Loading</a>
+                                    </li>
+                                    <li>
+                                        <a href="page_ui_preloader.html">Page Preloader</a>
+                                    </li>
+                                    <li>
+                                        <a href="page_ui_color_themes.html">Color Themes</a>
+                                    </li>
+                                </ul>
+                            </li>
+                            
+                          
+                           
+                                </ul>
+                            </li>
+                        </ul>
+                        <!-- END Sidebar Navigation -->
+
+                       
+                    </div>
+                    <!-- END Sidebar Content -->
+                </div>
+                <!-- END Wrapper for scrolling functionality -->
+            </div>
+            <!-- END Main Sidebar -->
+
+            <!-- Main Container -->
+            <div id="main-container">
+              
+                <header class="navbar navbar-default">
+                    <!-- Left Header Navigation -->
+                    <ul class="nav navbar-nav-custom">
+                        <!-- Main Sidebar Toggle Button -->
+                        <li>
+                            <a href="javascript:void(0)" onclick="App.sidebar('toggle-sidebar');">
+                                <i class="fa fa-bars fa-fw"></i>
+                            </a>
+                        </li>
+                        <!-- END Main Sidebar Toggle Button -->
+
+                        <!-- Template Options -->
+                        <!-- Change Options functionality can be found in js/app.js - templateOptions() -->
+                        <li class="dropdown">
+                            <a href="javascript:void(0)" class="dropdown-toggle" data-toggle="dropdown">
+                                <i class="gi gi-settings"></i>
+                            </a>
+                            <ul class="dropdown-menu dropdown-custom dropdown-options">
+                                <li class="dropdown-header text-center">Header Style</li>
+                                <li>
+                                    <div class="btn-group btn-group-justified btn-group-sm">
+                                        <a href="javascript:void(0)" class="btn btn-primary" id="options-header-default">Light</a>
+                                        <a href="javascript:void(0)" class="btn btn-primary" id="options-header-inverse">Dark</a>
+                                    </div>
+                                </li>
+                                <li class="dropdown-header text-center">Page Style</li>
+                                <li>
+                                    <div class="btn-group btn-group-justified btn-group-sm">
+                                        <a href="javascript:void(0)" class="btn btn-primary" id="options-main-style">Default</a>
+                                        <a href="javascript:void(0)" class="btn btn-primary" id="options-main-style-alt">Alternative</a>
+                                    </div>
+                                </li>
+                                <li class="dropdown-header text-center">Main Layout</li>
+                                <li>
+                                    <button class="btn btn-sm btn-block btn-primary" id="options-header-top">Fixed Side/Header (Top)</button>
+                                    <button class="btn btn-sm btn-block btn-primary" id="options-header-bottom">Fixed Side/Header (Bottom)</button>
+                                </li>
+                                <li class="dropdown-header text-center">Footer</li>
+                                <li>
+                                    <div class="btn-group btn-group-justified btn-group-sm">
+                                        <a href="javascript:void(0)" class="btn btn-primary" id="options-footer-static">Default</a>
+                                        <a href="javascript:void(0)" class="btn btn-primary" id="options-footer-fixed">Fixed</a>
+                                    </div>
+                                </li>
+                            </ul>
+                        </li>
+                        <!-- END Template Options -->
+                    </ul>
+                    <!-- END Left Header Navigation -->
+
+                   
+                    <!-- Right Header Navigation -->
+                    <ul class="nav navbar-nav-custom pull-right">
+                        <!-- Alternative Sidebar Toggle Button -->
+                        <li>
+                            <!-- If you do not want the main sidebar to open when the alternative sidebar is closed, just remove the second parameter: App.sidebar('toggle-sidebar-alt'); -->
+                            <a href="javascript:void(0)" onclick="App.sidebar('toggle-sidebar-alt', 'toggle-other');">
+                                <i class="gi gi-share_alt"></i>
+                                <span class="label label-primary label-indicator animation-floating">4</span>
+                            </a>
+                        </li>
+                        <!-- END Alternative Sidebar Toggle Button -->
+
+                        <!-- User Dropdown -->
+                        <li class="dropdown">
+                            <a href="javascript:void(0)" class="dropdown-toggle" data-toggle="dropdown">
+                                <img src="img/placeholders/avatars/avatar2.jpg" alt="avatar"> <i class="fa fa-angle-down"></i>
+                            </a>
+                            <ul class="dropdown-menu dropdown-custom dropdown-menu-right">
+                                 
+                                <li>
+                                    
+                                    <a href="login.html"><i class="fa fa-ban fa-fw pull-right"></i> Logout</a>
+                                </li>
+                                
+                            </ul>
+                        </li>
+                        <!-- END User Dropdown -->
+                    </ul>
+                    <!-- END Right Header Navigation -->
+                </header>
+                <!-- END Header -->
+
+                <!-- Page content -->
+                <div id="page-content">
+                    <!-- Datatables Header -->
+                    <div class="content-header">
+                        <div class="header-section">
+                            <h1>
+                                <i class="fa fa-table"></i>Master Siswa<br><small>Tabel Master Data Siswa</small>
+                            </h1>
+                        </div>
+                    </div>
+                    <ul class="breadcrumb breadcrumb-top">
+                        <li>Tables</li>
+                        <li><a href="">Datatables</a></li>
+                    </ul>
+                    <!-- END Datatables Header -->
+
+                    <!-- Datatables Content -->
+                    <div class="block full">
+                        <div class="block-title">
+                            <h2><strong>Data Master</strong>  </h2>
+                        </div>
+                         
+                        <div class="table-responsive">
+                            <table id="example-datatable" class="table table-vcenter table-condensed table-bordered">
+                                <thead>
+                                    <tr>
+                                        <th class="text-center">ID</th>
+                                        <th class="text-center"><i class="gi gi-user"></i></th>
+                                        <th>Client</th>
+                                        <th>Email</th>
+                                        <th>Subscription</th>
+                                        <th class="text-center">Actions</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <td class="text-center">1</td>
+                                        <td class="text-center"><img src="img/placeholders/avatars/avatar5.jpg" alt="avatar" class="img-circle"></td>
+                                        <td><a href="javascript:void(0)">client1</a></td>
+                                        <td>client1@company.com</td>
+                                        <td><span class="label label-primary">Personal</span></td>
+                                        <td class="text-center">
+                                            <div class="btn-group">
+                                                <a href="javascript:void(0)" data-toggle="tooltip" title="Edit" class="btn btn-xs btn-default"><i class="fa fa-pencil"></i></a>
+                                                <a href="javascript:void(0)" data-toggle="tooltip" title="Delete" class="btn btn-xs btn-danger"><i class="fa fa-times"></i></a>
+                                            </div>
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td class="text-center">2</td>
+                                        <td class="text-center"><img src="img/placeholders/avatars/avatar3.jpg" alt="avatar" class="img-circle"></td>
+                                        <td><a href="javascript:void(0)">client2</a></td>
+                                        <td>client2@company.com</td>
+                                        <td><span class="label label-warning">Trial</span></td>
+                                        <td class="text-center">
+                                            <div class="btn-group">
+                                                <a href="javascript:void(0)" data-toggle="tooltip" title="Edit" class="btn btn-xs btn-default"><i class="fa fa-pencil"></i></a>
+                                                <a href="javascript:void(0)" data-toggle="tooltip" title="Delete" class="btn btn-xs btn-danger"><i class="fa fa-times"></i></a>
+                                            </div>
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td class="text-center">3</td>
+                                        <td class="text-center"><img src="img/placeholders/avatars/avatar9.jpg" alt="avatar" class="img-circle"></td>
+                                        <td><a href="javascript:void(0)">client3</a></td>
+                                        <td>client3@company.com</td>
+                                        <td><span class="label label-success">VIP</span></td>
+                                        <td class="text-center">
+                                            <div class="btn-group">
+                                                <a href="javascript:void(0)" data-toggle="tooltip" title="Edit" class="btn btn-xs btn-default"><i class="fa fa-pencil"></i></a>
+                                                <a href="javascript:void(0)" data-toggle="tooltip" title="Delete" class="btn btn-xs btn-danger"><i class="fa fa-times"></i></a>
+                                            </div>
+                                        </td>
+                                    </tr>
+                                   
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                    <!-- END Datatables Content -->
+                </div>
+                <!-- END Page Content -->
+
+                <!-- Footer -->
+                <footer class="clearfix">
+                     
+                    <div class="pull-left">
+                        <span id="year-copy"></span> &copy; <a href="" target="_blank">STIKOM & UHAMKA</a>
+                    </div>
+                </footer>
+                <!-- END Footer -->
+            </div>
+            <!-- END Main Container -->
+        </div>
+        <!-- END Page Container -->
+
+        <!-- Scroll to top link, initialized in js/app.js - scrollToTop() -->
+        <a href="#" id="to-top"><i class="fa fa-angle-double-up"></i></a>
+
+        
+
+        <!-- Include Jquery library from Google's CDN but if something goes wrong get Jquery from local file (Remove 'http:' if you have SSL) -->
+        <script src="asset/jquery.min.js"></script>
+            <!-- Bootstrap.js, Jquery plugins and Custom JS code -->
+        <script src="asset/js/vendor/bootstrap.min.js"></script>
+        <script src="asset/js/plugins.js"></script>
+        <script src="asset/js/app.js"></script>
+
+        <!-- Load and execute javascript code used only in this page -->
+        <script src="asset/js/pages/tablesDatatables.js"></script>
+        <script>$(function(){ TablesDatatables.init(); });</script>
+    </body>
+</html>
